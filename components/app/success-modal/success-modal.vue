3e590e41c97609c28d94ff366def85e175a42958
Karen Grigoryan <kgrigoryan@dsnci.com>
2023-06-04 13:25:44 +0300

    init

--- components/app/success-modal/success-modal.vue
+++ components/app/success-modal/success-modal.vue
@@ -5,6 +5,7 @@
         img.cross._cross(src="../../../assets/img/cross.svg", alt="" @click="setSuccessModalStatus(false)")
         ._inner(ref="inner")
           h3._title Ура, твой пользователь активирован!
-          h4._title Успехов тебе! Не забудь войти в свой профиль!
+          h4._title Успехов тебе!
+          h4._title Не забудь войти в свой профиль!
           button.auth-modal__btn.-small.btn(@click="setSuccessModalStatus(false)") Спасибо
 </template>
